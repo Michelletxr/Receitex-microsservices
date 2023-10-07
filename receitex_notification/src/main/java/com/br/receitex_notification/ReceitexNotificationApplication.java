e5e46b7e7c339f58b324ec4f584a89dd2cd866af
Michelle Teixeira <michelle.teixeira.124@ufrn.edu.br>
2023-10-07 10:43:02 -0300

    add docker-compose

--- receitex_notification/src/main/java/com/br/receitex_notification/ReceitexNotificationApplication.java
+++ receitex_notification/src/main/java/com/br/receitex_notification/ReceitexNotificationApplication.java
@@ -2,8 +2,10 @@ package com.br.receitex_notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ReceitexNotificationApplication {
 
 	public static void main(String[] args) {
--- receitex_register/src/main/java/com/br/receitex_register/ReceitexRegisterApplication.java
+++ receitex_register/src/main/java/com/br/receitex_register/ReceitexRegisterApplication.java
@@ -2,8 +2,10 @@ package com.br.receitex_register;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ReceitexRegisterApplication {
 
 	public static void main(String[] args) {
