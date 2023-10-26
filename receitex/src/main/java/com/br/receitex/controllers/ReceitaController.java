7b8e8f10ff5c67715bad9bcfe73ae3eead96ef49
unknown <nandagb23@gmail.com>
2023-10-25 22:52:28 -0300

    adicionando permissao aos endpoints

--- receitex/src/main/java/com/br/receitex/controllers/ReceitaController.java
+++ receitex/src/main/java/com/br/receitex/controllers/ReceitaController.java
@@ -14,26 +14,31 @@ public class ReceitaController {
 
     @Autowired
     ReceitaService receitaService;
+    @CrossOrigin(origins = "http://localhost:5173")
     @PostMapping("/receita")
     public Documento createDocumento(@RequestBody Receita receita){
        return receitaService.createDocumento(receita);
     }
 
+    @CrossOrigin(origins = "http://localhost:5173")
     @GetMapping("/receita")
     public Receita getDocumentoById(@RequestParam Long id){
         return receitaService.getDocumentoById(id);
     }
 
+    @CrossOrigin(origins = "http://localhost:5173")
     @GetMapping("/receitas")
     public List<Receita> getAllDocumentos(){
         return receitaService.getAllDocumentos();
     }
 
+    @CrossOrigin(origins = "http://localhost:5173")
     @DeleteMapping("receita")
     public void deleteReceitaById(@RequestParam Long id){
         receitaService.deleteReceitaById(id);
     }
 
+    @CrossOrigin(origins = "http://localhost:5173")
     @DeleteMapping("deletar_receita")
     public void deleteReceita(@RequestBody Receita receita){
         receitaService.deleteReceita(receita);
